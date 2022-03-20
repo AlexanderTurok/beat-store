b1971c563b84f6be2bc782f077dbdd06aae03013
AlexanderTurok <alexander.turok2004@gmail.com>
2022-03-20 19:57:55 +0200

    add menu icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,21 @@
 
 import { Link } from 'react-router-dom';
+import { useState, useEffect, useCallback } from "react";
+
 import Menu from "./Menu";
 import Search from "./Search";
 import CartImage from '../images/cart.png';
+
+
+function MenuIcon() {
+  return (
+    <div className= "menu-icon">
+      <div className='menu-icon-bar'></div>
+      <div className='menu-icon-bar'></div>
+      <div className='menu-icon-bar'></div>
+    </div>
+  );
+}
 
 function Title() {
     return(
@@ -28,19 +41,36 @@ function Title() {
   }
 
 function NavBar({ price }) {
+  const [width, setWidth] = useState(null);
+  
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const handleWindowResize = useCallback(event => {
+    setWidth(window.innerWidth);
+  }, []); 
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowResize);
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, [handleWindowResize]);
+
   return (
     <div className='header'>
       <div className='header-col'>
         <Title />
       </div>
       <div className='header-col'>
-        <Menu />
+        <Menu closeMobileMenu={closeMobileMenu}/>
         <Shopping price={price}/>
         <Loggining />
         <Search />
+        {width <= 1027 && <MenuIcon />}
       </div>
     </div>    
   )
 }
 
-export default NavBar
+export default NavBar;
