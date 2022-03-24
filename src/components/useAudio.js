1d632c2c8f32fd0d51f08b5c4c693356e3c71a01
AlexanderTurok <alexander.turok2004@gmail.com>
2022-03-24 12:18:59 +0200

    remove white spaces in useAudio hook

--- src/components/useAudio.js
+++ src/components/useAudio.js
@@ -2,22 +2,22 @@
 import { useState, useEffect } from "react";
 
 function useAudio(url) {
-    const [audio] = useState(new Audio(url));
-    const [playing, setPlaying] = useState(false);
-    const toggle = () => {setPlaying(!playing);}
+  const [audio] = useState(new Audio(url));
+  const [playing, setPlaying] = useState(false);
+  const toggle = () => {setPlaying(!playing);}
   
-    useEffect(() => {
-      playing ? audio.play() : audio.pause();
-      }, [playing]);
+  useEffect(() => {
+    playing ? audio.play() : audio.pause();
+  }, [playing]);
   
-    useEffect(() => {
-      audio.addEventListener('ended', () => setPlaying(false));
-      return () => {
-        audio.removeEventListener('ended', () => setPlaying(false));
-      };
-    }, []);
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, []);
   
-    return [playing, toggle];
-  };
+  return [playing, toggle];
+};
 
-  export default useAudio;
+export default useAudio;
