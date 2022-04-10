8c35bd8f2fb1c14f31752b51f97d5ee9f09291ca
AlexanderTurok <alexander.turok2004@gmail.com>
2022-04-10 19:10:19 +0300

    refactor footer to few components

---
+++ src/components/footer/TermsPolicy.js
@@ -0,0 +1,16 @@
+
+
+function TermsPolicy() {
+  return(
+    <div  className="footer-col">
+      <p id="none">--none--</p>
+      <div className="terms-policy">
+        <a>TERMS OF USE</a>
+        <p className="dot">{'\u2B24'}</p>
+        <a>PRIVACY POLICY</a>
+      </div>
+    </div>
+  );
+}
+
+export default TermsPolicy;
