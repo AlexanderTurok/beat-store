83691f577a77c1e72d1ad53b1742b121ddae982e
AlexanderTurok <alexander.turok2004@gmail.com>
2022-04-14 11:36:20 +0300

    setCurrentSong instead of sending serchItem to visualizationSection

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,10 +3,9 @@ import { combineReducers } from "redux";
 
 import apiReducer from "./apiData/apiReducer";
 import playerReducer from "./audioPlayer/playerReducer";
-import searchItemReducer from "./searchItem/searchItemReducer";
+
 
 export const rootReducer = combineReducers({
   items: apiReducer,
-  searchItem: searchItemReducer,
   player: playerReducer
 });
